bc9694f94423d6b96b2e2e8c138def6af51d1282
Takumaron <tennis.takumax@gmail.com>
2020-04-29 16:27:23 +0900

    domainモデル定義からConvert関数を削除

--- pkg/domain/entity/member.go
+++ pkg/domain/entity/member.go
@@ -1,23 +1,8 @@
 package entity
 
-import (
-	"todone/db/mysql/model"
-)
-
 type Member struct {
 	Member *User
 	Role   int
 }
 
 type MemberSlice []*Member
-
-func ConvertToMemberSliceEntity(userBoardSlice model.UsersBoardSlice) MemberSlice {
-	res := make(MemberSlice, 0, len(userBoardSlice))
-	for _, userBoard := range userBoardSlice {
-		res = append(res, &Member{
-			Member: ConvertToUserEntity(userBoard.R.User),
-			Role:   int(userBoard.Role),
-		})
-	}
-	return res
-}
